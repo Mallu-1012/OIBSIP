cba832520935816c6bfaf67d5d1b5ceb8e277826
J-Bhaskar <1602-20-737-305@vce.ac.in>
2025-11-28 15:09:58 +0530

    Student main

---
+++ src/java/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+        System.out.println("no of students");
+        int n=sc.nextInt();
+        Student[] students=new Student[n];
+        for(int i=0;i<n;i++){
+            System.out.println("enter name,id,marks");
+            String name=sc.next();
+            int id=sc.nextInt();
+            int marks=sc.nextInt();
+            students[i]=new Student(name,id,marks);
+        }
+        for(Student s:students){
+            System.out.println(s);
+        }
+    }
+}
